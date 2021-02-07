8e499281f1d152f80786f3ede3ec09676cc004fe
oikewll <oikewll@gmail.com>
2021-02-07 15:32:54 +0800

    rename dist目录

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,130 @@
 <template>
-	<div id="app" class="mod-speak">
-		<div class="txt">{{eyan}}</div>
-		<div class="by">—— {{from}}{{who}}</div>
-		<ul class="btn-list">
-			<li><a class="btn-normal btn-heart" href="javascript:;" @click="likeit()"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-xin"></use></svg><span class="n" v-if="num !== 0">{{num}}</span></a>
-			<span class="pop" v-if="poplike"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-xin"></use></svg></span>
-			</li>
-			<li><a class="btn-normal" href="javascript:;" @click="querydata()"><svg class="icon" :class="{rotate: loading}" aria-hidden="true"><use xlink:href="#icon-genghuan"></use></svg></a></li>
-			<li><a class="btn-normal" href="javascript:;" @click="copytxt()"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-fuzhi"></use></svg></a>
-			<span class="hint" v-if="copyed">复制成功</span>
-			</li>
-		</ul>
-	</div>
+    <div id="app" class="mod-speak">
+        <div class="txt">{{ eyan }}</div>
+        <div class="by">—— {{ from }}{{ who }}</div>
+        <ul class="btn-list">
+            <li>
+                <a
+                    class="btn-normal btn-heart"
+                    href="javascript:;"
+                    @click="likeit()"
+                    ><svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-xin"></use></svg
+                    ><span class="n" v-if="num !== 0">{{ num }}</span></a
+                >
+                <span class="pop" v-if="poplike"
+                    ><svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-xin"></use></svg
+                ></span>
+            </li>
+            <li>
+                <a class="btn-normal" href="javascript:;" @click="querydata()"
+                    ><svg
+                        class="icon"
+                        :class="{ rotate: loading }"
+                        aria-hidden="true"
+                    >
+                        <use xlink:href="#icon-genghuan"></use></svg
+                ></a>
+            </li>
+            <li>
+                <a class="btn-normal" href="javascript:;" @click="copytxt()"
+                    ><svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-fuzhi"></use></svg
+                ></a>
+                <span class="hint" v-if="copyed">复制成功</span>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-	name: 'app',
-	data () {
-		return {
-			loading: true,
-			poplike: false,
-			copyed: false,
-			eyan: '加载中…',
-			from: '',
-			who: '',
-			num: 0
-		}
-	},
-	mounted: function() {
+    name: "app",
+    data() {
+        return {
+            loading: true,
+            poplike: false,
+            copyed: false,
+            eyan: "加载中…",
+            from: "",
+            who: "",
+            num: 0,
+        };
+    },
+    mounted: function () {
+        this.querydata();
+    },
+    methods: {
+        querydata() {
+            let that = this;
 
-		this.querydata();
+            that.loading = true;
+            that.eyan = "加载中…";
+            that.who = "";
+            that.num = 0; //以后可以存储一个like的统计数
 
-	},
-	methods: {
-		querydata(){
-			let that = this;
+            fetch("//v1.hitokoto.cn/")
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (json) {
+                    that.eyan = json.hitokoto;
+                    that.from = "《" + json.from + "》";
+                    // that.who = json.creator;
+                    that.loading = false;
 
-			that.loading = true;
-			that.eyan = '加载中…';
-			that.who = '';
-			that.num = 0;  //以后可以存储一个like的统计数
+                    console.log("parsed json", json);
+                })
+                .catch(function (ex) {
+                    console.log("Err:", ex);
+                });
+        },
+        likeit() {
+            if (this.poplike) return;
 
-			fetch('//v1.hitokoto.cn/')
-			.then(function(response) {
-				return response.json()
-			}).then(function(json) {
-				that.eyan = json.hitokoto;
-				that.from = '《'+json.from+'》';
-				// that.who = json.creator;
-				that.loading = false;
+            let that = this,
+                num = that.num;
 
-				console.log('parsed json', json)
-			}).catch(function(ex) {
-				console.log('Err:', ex)
-			})
-		},
-		likeit(){
-			if (this.poplike) return;
+            num++;
 
-			let that = this,
-				num = that.num;
+            that.poplike = true;
+            that.num = num;
 
-			num++;
+            setTimeout(function () {
+                that.poplike = false;
+            }, 500);
+        },
+        copytxt() {
+            let that = this;
 
-			that.poplike = true;
-			that.num = num;
+            if (this.copyed) return;
 
-			setTimeout(function(){
-				that.poplike = false;
-			}, 500);
-		},
-		copytxt(){
-			let that = this;
+            // 动态创建 input 元素
+            var aux = document.createElement("input");
+            // 获得需要复制的内容
+            aux.setAttribute("value", that.eyan);
+            // 添加到 DOM 元素中
+            document.body.appendChild(aux);
+            // 执行选中
+            // 注意: 只有 input 和 textarea 可以执行 select() 方法.
+            aux.select();
+            // 获得选中的内容
+            var content = window.getSelection().toString();
+            // 执行复制命令
+            document.execCommand("copy");
+            // 将 input 元素移除
+            document.body.removeChild(aux);
 
-			if (this.copyed) return;
-
-			// 动态创建 input 元素
-			var aux = document.createElement("input");
-			// 获得需要复制的内容
-			aux.setAttribute("value", that.eyan);
-			// 添加到 DOM 元素中
-			document.body.appendChild(aux);
-			// 执行选中
-			// 注意: 只有 input 和 textarea 可以执行 select() 方法.
-			aux.select();
-			// 获得选中的内容
-			var content = window.getSelection().toString();
-			// 执行复制命令
-			document.execCommand("copy");
-			// 将 input 元素移除
-			document.body.removeChild(aux);
-
-			// hui.toast('复制成功');
-			that.copyed = true;
-			console.log('复制成功：'+that.eyan);
-			setTimeout(function(){
-				that.copyed = false;
-			}, 2200);
-		}
-	}
-}
+            // hui.toast('复制成功');
+            that.copyed = true;
+            console.log("复制成功：" + that.eyan);
+            setTimeout(function () {
+                that.copyed = false;
+            }, 2200);
+        },
+    },
+};
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
+console.log(123)
 import Vue from 'vue'
 import App from './App.vue'
 import 'whatwg-fetch'
 import './assets/css/style.css'
 
 new Vue({
-	el: '#app',
-	render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 });
